63dbe4fd86e65c5eaeaad8118c883a20e849dd32
Pavel Akulichev <paveltalking@gmail.com>
2020-09-12 15:10:46 +0500

    RC-X Update

--- client/moonloader/SLMultiplayer.lua
+++ client/moonloader/SLMultiplayer.lua
@@ -13,7 +13,7 @@ require 'moonloader' -- SL:MP based on MoonLoader by FYP
 imgui = require 'mimgui' -- imgui the best
 bit = require 'bit' -- client uses Bit to encode data
 socket = require 'socket' -- client uses LuaSocket
-udp = socket.udp() -- client uses UDP method to send data
+--udp = socket.udp() -- client uses UDP method to send data
 memory = require 'memory' -- some memory focuses, okay?
 ffi = require 'ffi' -- one more focus
 FA = require 'FA5Pro' -- just icons
@@ -43,11 +43,23 @@ function GetArguments()
   return args
 end
 
-local startMultiplayer = false
+startMultiplayer = false
+conData =
+{
+  host = nil,
+  port = nil,
+  name = nil
+}
 local args = GetArguments()
 for i, v in pairs(args) do
   if v[1] == 'multiplayer' then
     startMultiplayer = true
+  elseif v[1] == 'h' then
+    conData.host = v[2]:gsub('%s', '')
+  elseif v[1] == 'p' then
+    conData.port = v[2]:gsub('%s', '')
+  elseif v[1] == 'n' then
+    conData.name = v[2]:gsub('%s', '')
   end
 end
 
@@ -58,7 +70,6 @@ end
 
 gMenuPatch = true
 if memory.getuint32(0xC8D4C0, false) < 9 then
-  -- if game isn`t loaded now we will make some patches
   gMenuPatch = false -- notify client that game is not loaded
   pcall(memory.copy, 0x866CCC, memory.strptr("slmp_load"..string.char(0x0)), 10)
   pcall(memory.fill, 0x747483, 0x90, 6, true) -- bypass vids
@@ -66,22 +77,25 @@ if memory.getuint32(0xC8D4C0, false) < 9 then
   pcall(memory.write, 0x5B8E6A, 134217728, 4, true) -- multipling streaming value
 end
 
+local SLModulesDir = getGameDirectory() .. '\\moonloader\\SLModules\\'
+package.path = package.path .. ';' .. SLModulesDir .. '?.lua;' .. SLModulesDir .. '?.luac'
+package.cpath = package.cpath .. ';' .. SLModulesDir .. '?.dll'
+
 modules = getWorkingDirectory() .. '/SLModules' -- some server modules located there
-dofile(modules..'/utilities/encoder.lua') -- data to bytes encoder by Akionka
-dofile(modules..'/utilities/snippets.lua') -- useful snippets and functions
-dofile(modules..'/bitstream.lua') -- method to work with data using encoder
-dofile(modules..'/defines.lua') -- just defines, nothing interesting
-dofile(modules..'/networking.lua') -- sending and receiving data from server
-dofile(modules..'/initialization.lua') -- some operations to get client ready
+require('utilities.encoder') -- data to bytes encoder by Akionka
+require('utilities.snippets') -- useful snippets and functions
+require('bitstream') -- method to work with data using encoder
+require('defines') -- just defines, nothing interesting
+require('networking') -- sending and receiving data from server
+require('initialization') -- some operations to get client ready
 
-local dir = ffi.C.malloc(40) ffi.copy(dir, "moonloader\\SLModules\\resources\\sa-files\0")
-local name_script = ffi.C.malloc(11) ffi.copy(name_script, "script.scm\0")
-local name_loadscreen = ffi.C.malloc(15) ffi.copy(name_loadscreen, "loadscreen.txd\0")
-Patches.setMainScriptPath(dir, name_script)
-Patches.setLoadScreensTxd(dir, name_loadscreen)
-
-udp:settimeout(0) -- client will not disconnect server with timeout
-udp:setpeername('localhost', 0) -- set local IP and unknown PORT
+if memory.getuint32(0xC8D4C0, false) < 9 then
+  local dir = ffi.C.malloc(5) ffi.copy(dir, "moonloader\\SLModules\\resources\\sa-files\0")
+  local name_script = ffi.C.malloc(11) ffi.copy(name_script, "script.scm\0")
+  local name_loadscreen = ffi.C.malloc(15) ffi.copy(name_loadscreen, "loadscreen.txd\0")
+  Patches.setMainScriptPath(dir, name_script)
+  Patches.setLoadScreensTxd(dir, name_loadscreen)
+end
 
 function main() -- moonloader script header function
   print('SL:MP Initialization Finished')
@@ -96,8 +110,55 @@ function main() -- moonloader script header function
 
   lua_thread.create(NetworkLoop)
   lua_thread.create(RenderLoop)
+
+  DiscordRPC.Discord_Initialize("754014924543623240", 0, 0, "")
+	dRPC.startTimestamp = os.time()
+
+  if conData.host and conData.port and conData.name then
+
+    setTimeOfDay(8, 0); forceWeatherNow(1)
+    displayHud(false); displayRadar(false)
+    setCharProofs(PLAYER_PED, true, true, true, true, true)
+    setCharCoordinates(PLAYER_PED, 1775.92, -1506.60, 0.0)
+    setCharVisible(PLAYER_PED, false)
+    setPlayerControl(PLAYER_HANDLE, false)
+    setFixedCameraPosition(1775.92, -1506.60, 190.0, 0.0, 0.0, 0.0)
+    pointCameraAtPoint(1387.26, -1122.99, 248.52, 2)
+
+    Game:addChatMessage('SL:MP ' .. General.VersionS .. ' started.', 0xCFCFCFFF)
+    Game:addChatMessage('Connecting to ' .. conData.host .. ':' .. conData.port .. '...', 0xCFCFCFFF)
+    wait(10000)
+    print(conData.host, conData.port, conData.name)
+    Socket:init(conData.host, conData.port)
+    --[[local bs = BitStream:new()
+    sendPacket(PACKET.PING_SERVER, false, bs)]]
+    Client:connect(conData.name)
+    Config.playerName = conData.name
+    Config.serverAddress = tostring(conData.host .. ':' .. conData.port)
+  end
+
   while true do
     wait(0)
+
+    --local x, y, z = getCharCoordinates(PLAYER_PED)
+    --Game:addChatMessage(tostring(x..','..y..','..z), 0xFFFFFFFF)
+
+    dRPC.largeImageKey = 'game'
+		if Player.GameState == GAMESTATE.CONNECTED then
+			dRPC.largeImageText = Config.serverAddress
+			dRPC.details = 'Playing ' .. ClientData.sName
+			dRPC.state = ClientData.sWebsite .. ' (' .. ClientData.sPlayers[1] .. ' of ' .. ClientData.sPlayers[2] .. ')'
+			dRPC.partySize = ClientData.sPlayers[1]
+			dRPC.partyMax = ClientData.sPlayers[2]
+			dRPC.partyId = Config.serverAddress
+		else
+			dRPC.largeImageText = 'In Menu'
+			dRPC.details = 'Idle'
+			dRPC.state = ''
+		end
+
+    DiscordRPC.Discord_UpdatePresence(dRPC)
+
   end
 end
 
@@ -114,17 +175,16 @@ end
 
 function onScriptTerminate(script, quitGame)
   if script == thisScript() then
-    ffi.C.free(dir)
+    --[[ffi.C.free(dir)
     ffi.C.free(name_script)
-    ffi.C.free(name_loadscreen)
-
+    ffi.C.free(name_loadscreen)]]
+    -- IT CRASH FUCKING GAME!
     Client:disconnect(true)
     local file = io.open(configFolder..'\\conf.json', 'w+')
     if file then
       file:write(encodeJson(Config))
       file:close()
     end
-    udp:close()
   end
 end
 
